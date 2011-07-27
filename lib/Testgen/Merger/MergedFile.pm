d75beb739ac5772e8384d8b08af69dabf9669594
Syohei YOSHIDA <syohex@gmail.com>
2011-07-27 23:39:25 +0900

    move parsing method to Parser class

--- lib/Testgen/Merger/MergedFile.pm
+++ lib/Testgen/Merger/MergedFile.pm
@@ -5,6 +5,7 @@ use warnings;
 use Carp ();
 
 use Testgen::Runner::Command;
+use Testgen::Parser;
 
 sub new {
     my ($class, %args) = @_;
@@ -14,108 +15,33 @@ sub new {
     }
 
     bless {
-        functions    => [],
+        fragments    => [],
         header       => {},
         pseudo_mains => [],
+        files        => {},
         %args,
     }, $class;
 }
 
 sub add {
-    my ($self, $file) = @_;
+    my ($self, $file, $prefix) = @_;
+
     my $compiler = $self->{compiler};
+    my ($preprocessed, $stderr) = $compiler->preprocess($file);
 
-    my ($preprocessed, $stderr) = $self->{compiler}->preprocess($file);
+    my $parser   = Testgen::Parser->new(
+        lang     => 'c',
+        compiler => $compiler->name,
+    );
+    my $c_source = $parser->remove_preprocessor_directives($preprocessed);
+    my $modified = $parser->prepend_to_identifier($c_source, $prefix);
 
-    my $prefix = _function_prefix($file);
-    my $c_source = _remove_preprocessor_directives($preprocessed);
-    push @{$self->{functions}}, _prepend_to_identifiers($c_source, $prefix);
-    map { $self->{header}->{$_} = 1 } _find_lacking_headers($stderr, $compiler->name);
+    my @missing_headers = $parser->find_missing_headers($stderr);
+
+    push @{$self->{fragments}}, $modified;
+    map { $self->{header}->{$_} = 1 } @missing_headers;
 
     push @{$self->{pseudo_mains}}, "${prefix}_main";
-}
-
-my @RESERVED = (qw/
-    auto     _Bool      break       case
-    char     _Complex   const       continue
-    default  do         double      else
-    enum     extern     float       for
-    goto     if         _Imaginary  inline
-    int      long       register    restrict
-    return   short      signed      sizeof
-    static   struct     switch      typedef
-    union    unsigned   void        volatile
-    while
-/);
-my %reserved_word = map { $_ => 1 } @RESERVED;
-
-my @IGNORES = qw/ printf /;
-my %ignore_word  = map { $_ => 1 } @IGNORES;
-
-my $identifier_re = qr{ [a-zA-Z_] (?: [a-zA-Z0-9_]+)? }xms;
-
-## Based on $Regexp::Common::RE{num}{real}
-my $real_num_re = qr{(?:(?i)(?:[+-]?)(?:(?=[.]?[0-9])(?:[0-9]*)(?:(?:[.])(?:[0-9]{0,}))?)(?:(?:[eE])(?:(?:[+-]?)(?:[0-9]+))))};
-
-## Based on $Regexp::Common::RE{num}{int}
-my $octal_re   = qr{(?:(?:[+-]?)(?:[0-7]+))};
-my $decimal_re = qr{(?:(?:[+-]?)(?:[0-9]+))};
-my $hex_re     = qr{(?:(?:[+-]?)0[xX](?:[0-9a-fA-F]+))};
-my $int_re = qr{ (?: $hex_re | $octal_re | $decimal_re ) (?: (?i)(?:ul?l?|ll?|d?f) )? }x;
-
-my $num_re = qr{ (?: $real_num_re (?: (?i)(?:d?f) )? | $int_re ) }xmso;
-
-## Based on $Regexp::Common::RE{quoted}
-my $quoted = qr{(?:(?:\")(?:[^\\\"]*(?:\\.[^\\\"]*)*)(?:\")|(?:\')(?:[^\\\']*(?:\\.[^\\\']*)*)(?:\'))};
-
-my $symbol_re = qr{ [\\+\-*&^~/%()\{\}\[\];=?|:><.!,] }xmso;
-
-sub _prepend_to_identifiers {
-    my ($file_str, $prefix) = @_;
-    my $orig = $file_str;
-    my $replaced = '';
-
-    $file_str =~ s{ \s+ }{ }gxms;
-
-    my %cache;
-    while (1) {
-        while ($orig =~ m{ \G ( \s| $symbol_re+ | $quoted | $num_re ) }gxmsc) {
-            $replaced .= $1;
-            # read skip
-        }
-
-        if ($orig =~ m{\G ( $identifier_re ) }gxmsc) {
-            my $identifier = $1;
-
-            if (exists $reserved_word{$identifier}
-                || exists $ignore_word{$identifier}) {
-                $replaced .= $identifier;
-            } else {
-                $replaced .= "${prefix}_${identifier}";
-            }
-
-        } elsif ($orig =~ m{\G \z}gxmsc) {
-            last;
-        }
-    }
-
-    return $replaced;
-}
-
-sub _function_prefix {
-    my $file = shift;
-
-    $file =~ s{\.c$}{_}g;
-    $file =~ s{\.}{_}g;
-    $file =~ s{-}{_}g;
-
-    return $file;
-}
-
-sub _remove_preprocessor_directives {
-    my $str = shift;
-    $str =~ s{ ^ \# .*? $ }{}gxms;
-    return $str;
 }
 
 sub output_as_main_file {
@@ -123,7 +49,7 @@ sub output_as_main_file {
 
     my $headers_str     = join "\n", keys %{$self->{headers}};
     my $pseudo_main_str = join "\n", map { "$_();" } @{$self->{pseudo_mains}};
-    my $functions_str    = join "\n", @{$self->{functions}};
+    my $fragments_str   = join "\n", @{$self->{fragments}};
 
     open my $fh, '>', $file or Carp::croak("Can't open $file: $!");
 
@@ -141,7 +67,7 @@ $pseudo_main_str
     return 0;
 }
 
-$functions_str
+$fragments_str
 ...
 
     close $fh;
@@ -150,9 +76,8 @@ $functions_str
 sub output_as_sub_file {
     my ($self, $file) = @_;
 
-    my $headers_str     = join "\n", keys %{$self->{headers}};
-    my $pseudo_main_str = join ";\n", @{$self->{pseudo_mains}};
-    my $functions_str   = join "\n", @{$self->{functions}};
+    my $headers_str   = join "\n", keys %{$self->{headers}};
+    my $fragments_str = join "\n", @{$self->{fragments}};
 
     open my $fh, '>', $file or Carp::croak("Can't open $file: $!");
 
@@ -162,28 +87,9 @@ sub output_as_sub_file {
 #endif
 $headers_str
 
-$functions_str
+$fragments_str
 ...
     close $fh;
-}
-
-my %error_message = (
-    gcc => qr{
-        no \s+ include \s+ path \s+ in \s+ which \s+ to \s+ search \s+ for \s+
-        ([^.]+.h)
-    }ixmso,
-);
-
-sub _find_lacking_headers {
-    my ($str, $compiler) = @_;
-
-    my %header;
-    my $regexp = $error_message{$compiler};
-    while ($str =~ m{$regexp}g) {
-         $header{$1} = 1;
-    }
-
-    return keys %header;
 }
 
 1;
