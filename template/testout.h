6a2e1bbda65d1f1124d88a3edd2489ebef27b277
Takayuki Fukumoto <takayuki.fukumoto@kwansei.ac.jp>
2011-11-02 00:55:20 +0900

    change templates

--- template/testout.h
+++ template/testout.h
@@ -2,8 +2,8 @@
 #ifdef unix
 #include<stdio.h>
 #include<float.h>
-#define printok() printf("@OK@\n")
-#define printno() printf("@NG@\n")
+#define printok() write(1,"@OK@\n",5)
+#define printno() write(1,"@NG@\n",5)
 #define ABS(a) ((a > 0)? (a) : (-(a)))
 #define MAX(a, b) ((a > b)? (a) : (b))
 #define TGEN_FLT_EQ(a, b) ( ((a)==(b)) || ABS(((a) - (b))) / MAX( ABS(a), ABS(b) ) <= FLT_EPSILON )
