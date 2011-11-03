8846db4086c7c017a9327df31474e62fbe6d554c
Takayuki Fukumoto <takayuki.fukumoto@kwansei.ac.jp>
2011-11-04 08:24:37 +0900

    change testout.h

--- config/testout.h
+++ config/testout.h
@@ -3,6 +3,7 @@
 #include<float.h>
 #define printok() write(1,"@OK@\n",5)
 #define printno() write(1,"@NG@\n",5)
+#define printdiv() write(1,"====\n",5)
 #define ABS(a) ((a > 0)? (a) : (-(a)))
 #define MAX(a, b) ((a > b)? (a) : (b))
 #define TGEN_FLT_EQ(a, b) ( ((a)==(b)) || ABS(((a) - (b))) / MAX( ABS(a), ABS(b) ) <= FLT_EPSILON )
